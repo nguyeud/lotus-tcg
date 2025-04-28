e6fe980688f6d8c6903f32cc35affffd1699079d
nguyeud <nguyeud@duck.com>
2025-04-28 03:33:54 -0700

    style(initial-setup): :lipstick: Add Eslint and Prettier dependencies and configuration

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,7 @@
-const {getDefaultConfig} = require('expo/metro-config');
-const {withNativeWind} = require('nativewind/metro');
+/* eslint-env node */
+const { getDefaultConfig } = require('expo/metro-config');
+const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, {input: '@/app/global.css'});
+module.exports = withNativeWind(config, { input: '@/app/global.css' });
