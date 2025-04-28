3c64771eb823f19cc943620288a303c743e79f47
nguyeud <nguyeud@duck.com>
2025-04-28 03:39:12 -0700

    build(initial-setup): :heavy_plus_sign: Add react-native-reanimated library

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,12 @@
 /* eslint-env node */
+const {
+  wrapWithReanimatedMetroConfig,
+} = require('react-native-reanimated/metro-config');
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: '@/app/global.css' });
+module.exports = withNativeWind(wrapWithReanimatedMetroConfig(config), {
+  input: '@/app/global.css',
+});
